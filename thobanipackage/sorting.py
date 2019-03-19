5bc31ad414fbad12d3b37967398fc87599254e1a
Thobani Mthimkhulu <thobanimthimkhulu@gmail.com>
2019-03-19 16:46:41 +0200

    Fixing merge sort

--- thobanipackage/sorting.py
+++ thobanipackage/sorting.py
@@ -13,19 +13,38 @@ def bubble_sort(items):
 
 
 def merge_sort(items):
-   '''Return array of items, sorted in ascending order'''
-   len_sort = len(items)
-   # Base case. A list of size 1 is sorted.
+    """ Return array of items, sorted in ascending order """
+    if len(items) >1:
+        mid = len(items)//2 #Finding the mid of the array
+        L = items[:mid] # Dividing the array elements
+        R = items[mid:] # into 2 halves
 
-   if len_sort == 1:
-       return items
-   # Recursive case. Find midpoint of list
-   mid_point = int(len_sort / 2)
+        merge_sort(L) # Sorting the first half
+        merge_sort(R) # Sorting the second half
 
-   i1 = merge_sort(items[:mid_point]) # divide list into two halves
-   i2 = merge_sort(items[mid_point:]) # call merge_sort function on each half of list
+        i = j = k = 0
 
-   return merge(i1, i2)
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                items[k] = L[i]
+                i+=1
+            else:
+                items[k] = R[j]
+                j+=1
+            k+=1
+
+        # Checking if any element was left
+        while i < len(L):
+            items[k] = L[i]
+            i+=1
+            k+=1
+
+        while j < len(R):
+            items[k] = R[j]
+            j+=1
+            k+=1
+    return items
 
 
 
